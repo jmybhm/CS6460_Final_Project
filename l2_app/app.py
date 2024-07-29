f43531d77c7db99252b0cb201e3a762951c53ef8
Lauren Yoo <laurenyoo@Laurens-MacBook-Air.local>
2024-07-29 08:01:19 -0400

    adding discussion quesitons

--- l2_app/app.py
+++ l2_app/app.py
@@ -4,6 +4,60 @@ from movies import my_movie_list
 
 app = Flask(__name__)
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
 @app.route("/")
 @app.route("/mypage")
 def mypage():
@@ -27,7 +81,7 @@ def display_text(movie_name):
     file_path = os.path.join('static','files', file_name)
     with open(file_path, 'r') as file:
         content = file.read()
-    return render_template('moviescript.html', content=content)
+    return render_template('moviescript.html', movie_name= movie_name, content=content)
 
 @app.route("/mymovies/10_things_i_hate_about_you/vocabulary")
 def vocab():
